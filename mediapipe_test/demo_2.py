07e6253cd67faf2734be5f158e94d7c2149bf876
gosupraneeth <gosupraneeth322@gmail.com>
2022-03-01 13:46:30 +0530

    added model to demo_2.py

--- mediapipe_test/demo_2.py
+++ mediapipe_test/demo_2.py
@@ -4,6 +4,7 @@ import time
 import pickle
 import numpy as np
 import pyautogui
+from tensorflow import keras
 
 def main():
     cod_x = ""
@@ -23,7 +24,7 @@ def main():
 
     mpDraw = mp.solutions.drawing_utils
     landmarks = ["WRIST","THUMB_CMC","THUMB_MCP","THUMB_IP","THUMB_TIP","INDEX_FINGER_MCP","INDEX_FINGER_PIP","INDEX_FINGER_DIP","INDEX_FINGER_TIP","MIDDLE_FINGER_MCP","MIDDLE_FINGER_PIP","MIDDLE_FINGER_DIP","MIDDLE_FINGER_TIP","RING_FINGER_MCP","RING_FINGER_PIP","RING_FINGER_DIP","RING_FINGER_TIP","PINKY_MCP","PINKY_PIP","PINKY_DIP","PINKY_TIP"]
-    loaded_model = pickle.load(open('./../models/knn_model.sav', 'rb'))
+    loaded_model = keras.models.load_model('./../models/DL_model.h5')
     screenWidth, screenHeight = pyautogui.size()
     #from tensorflow import keras
     #loaded_model = keras.models.load_model('./../models/DL_model.h5')
@@ -51,8 +52,8 @@ def main():
         inp_row = []
         inp_row.append(row)
         pred_action = loaded_model.predict(inp_row)
-        #predict_action = classes[np.argmax(pred_action)]
-        predict_action = pred_action
+        predict_action = classes[np.argmax(pred_action)]
+        #predict_action = pred_action
         print(predict_action)
 
         img = cv2.flip(img, 1)
@@ -70,7 +71,7 @@ def main():
                 cod += " y: " + str(y_cod)
                 
                 cv2.putText(img,cod, (10,100), cv2.FONT_HERSHEY_PLAIN, 2, (255,0,255), 3)
-                cv2.putText(img,predict_action[0], (10,200), cv2.FONT_HERSHEY_PLAIN, 2, (255,0,255), 3)
+                cv2.putText(img,predict_action, (10,200), cv2.FONT_HERSHEY_PLAIN, 2, (255,0,255), 3)
                 
                 img = cv2.flip(img, 1)
 
